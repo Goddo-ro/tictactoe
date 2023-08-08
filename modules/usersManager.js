485184d739eba5910d273b9d7be38de79f024c81
Goddo-ro <r0manovic1@yandex.ru>
2023-08-08 14:58:14 +0300

    Add usersManager to get users names

---
+++ modules/usersManager.js
@@ -0,0 +1,28 @@
+export default function usersManager(error = "") {
+  const modalWindow = document.getElementById("modal-container");
+  const playerOneInput = document.getElementById("playerOneInput");
+  const playerTwoInput = document.getElementById("playerTwoInput");
+  const errorLabel = document.getElementById("errorLabel");
+  const saveBtn = document.getElementById("saveBtn");
+
+  if (error) {
+    errorLabel.innerText = 'Error: ' + error;
+    setTimeout(() => {
+      errorLabel.innerText = "";
+    }, 5000);
+  }
+
+  return new Promise((resolve, reject) => {
+    const handleClick = () => {
+      if (playerOneInput.value && playerTwoInput.value) {
+        modalWindow.style.display = "none";
+        saveBtn.removeEventListener("click", handleClick);
+        resolve([playerOneInput.value, playerTwoInput.value]);
+      } else {
+        reject("Enter players names");
+      }
+    }
+
+    saveBtn.addEventListener("click", handleClick);
+  })
+}
